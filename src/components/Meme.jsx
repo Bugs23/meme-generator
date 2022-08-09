8f6c6d9588c4a89c7573a7ddf9ef0b1a9df2c4bd
Raymond Urrutia <rurrutia@RUrruitaMBP.local>
2022-08-09 16:05:57 -0400

    Update our state to save the meme-related
    Update state to save the meme-related info

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -2,18 +2,30 @@ import React from "react"
 import memesData from "../memesData.js"
 
 function Meme() {
-    // Create new state called `memeImage` with a random meme image as default
-    const [memeImage, setMemeImage] = React.useState("http://i.imgflip.com/1bij.jpg")
+	const [meme, setMeme] = React.useState({
+		topText: "",
+		bottomText: "",
+		randomImage: "http://i.imgflip.com/1bij.jpg"
+	})
+
+	const [allMemeImages, getAllMemeImages] = React.useState(memesData)
 
 	function getMemeImage() {
 		// Get all the memes from the the memeasData file
-		const memesArray = memesData.data.memes
+		const memesArray = allMemeImages.data.memes
 		// See how many memes there are and pick a random number between 1 and the total number of memes
 		const randomNumber = Math.floor(Math.random()*memesArray.length)
+
+		const url = memesArray[randomNumber].url
 		
 		/* When the getMemeImage function is called, update the 
 		`memeImage` state to be the random chosen image URL */
-		setMemeImage(memesArray[randomNumber].url)
+		setMeme(prevMeme => ({
+			...prevMeme,
+			topText: prevMeme.topText,
+			bottomText: prevMeme.bottomText,
+			randomImage: url
+		}))
 
 	}
 
@@ -24,7 +36,9 @@ function Meme() {
                 <input className="form--input" type="text" placeholder="Bottom Text"/>
                 <button className="form--button" onClick={getMemeImage}>Get a new meme image 🖼</button>
 			</div>
-			<img className="meme--image" src={memeImage} />
+			<img className="meme--image" src={meme.randomImage} />
+			<h2>{meme.topText}</h2>
+			<h2>{meme.bottomText}</h2>
 		</main>
 	)
 }
